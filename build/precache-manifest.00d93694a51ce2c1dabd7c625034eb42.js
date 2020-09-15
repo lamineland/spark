f75b0708a34664e8d987cdbf5257b6bd21bf106a
Lamine Sané <aminesane18@gmail.com>
2020-09-15 13:16:08 +0000

    First build production

---
+++ build/precache-manifest.00d93694a51ce2c1dabd7c625034eb42.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77a681677a90416db7df8d89cb7e7ab0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "48a06e84dc762b4b47c1",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "3c2ce2709fdebe8ed28a",
+    "url": "/static/js/2.4acbd279.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.4acbd279.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "48a06e84dc762b4b47c1",
+    "url": "/static/js/main.d6777648.chunk.js"
+  },
+  {
+    "revision": "d2de885e343a34397603",
+    "url": "/static/js/runtime-main.aa0173df.js"
+  },
+  {
+    "revision": "2f546b3858eb252a37d7a64cb006b564",
+    "url": "/static/media/about_1.2f546b38.jpg"
+  },
+  {
+    "revision": "aedd873da8bdd433746676bc85283c47",
+    "url": "/static/media/hero_1.aedd873d.jpg"
+  },
+  {
+    "revision": "03d067fbeb9a2728e311b766869f9d45",
+    "url": "/static/media/img_1.03d067fb.jpg"
+  },
+  {
+    "revision": "04cea94ff3de2c3ff2bd4ad1e4379a1e",
+    "url": "/static/media/img_2.04cea94f.jpg"
+  },
+  {
+    "revision": "25984754db58a1fe2b939d195b786009",
+    "url": "/static/media/img_3.25984754.jpg"
+  },
+  {
+    "revision": "c9e3fee5edf8fcaa3e476838d9384d20",
+    "url": "/static/media/img_4.c9e3fee5.jpg"
+  },
+  {
+    "revision": "7692daf8060e987734113d4c98ffca46",
+    "url": "/static/media/img_5.7692daf8.jpg"
+  },
+  {
+    "revision": "9cd3792ce0f208978517ffe0d0a5ef89",
+    "url": "/static/media/img_6.9cd3792c.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "4a2652fa3ae8d7010b7ac536288efe8b",
+    "url": "/static/media/person_2.4a2652fa.jpg"
+  },
+  {
+    "revision": "41838d119c3c8e914949576b4728a063",
+    "url": "/static/media/person_3.41838d11.jpg"
+  },
+  {
+    "revision": "d9e79e2c80a71c349f37cf55f7be7689",
+    "url": "/static/media/person_4.d9e79e2c.jpg"
+  }
+]);
